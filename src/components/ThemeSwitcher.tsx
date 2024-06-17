66d3215634ecc112d8e52d65a0aecf043b3e132a
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-06-17 09:34:09 +0530

    refactor: Improve theme switcher UI and functionality

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -20,9 +20,11 @@ export function ThemeSwitcher() {
 
     return (
         <>
-        <button onClick={switchTheme} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-            Switch Theme
-        </button>
+        <label className="inline-flex items-center me-5 cursor-pointer bg-white dark:bg-gray-900">
+            <input type="checkbox" className="sr-only peer" checked={theme === "dark"} onClick={switchTheme} />
+            <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-2 peer-focus:ring-gray-800 dark:peer-focus:ring-gray-300 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-600"></div>
+            <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Switch to {(theme === "dark") ? "light":"dark"}</span>
+            </label>
         </>
     )
 }
