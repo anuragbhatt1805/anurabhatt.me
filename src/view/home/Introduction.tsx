84a97df8d57553b505d8a9079a964de3d6f507ed
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-06-24 00:57:33 +0530

    `refactor: Add Instagram link to Introduction component`

--- src/view/home/Introduction.tsx
+++ src/view/home/Introduction.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import { FaFacebookSquare, FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { FaFacebookSquare, FaGithubSquare, FaInstagramSquare, FaLinkedin } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import Typed from "typed.js";
@@ -58,9 +58,10 @@ export const Introduction = () => {
         </Link>
       </div>
       <div className="flex flex-wrap flex-row gap-3 my-5 mx-2">
-      <Link to='https://facebook.com/anurag.bhatt.52459/' target="_blank"><FaFacebookSquare size="2rem"/></Link>
       <Link to='https://www.linkedin.com/in/anurag-bhatt-846a241b8/' target="_blank"><FaLinkedin size="2rem"/></Link>
       <Link to='https://x.com/anuragbhatt1805' target="_blank"><FaSquareXTwitter size="2rem"/></Link>
+      <Link to='https://facebook.com/anurag.bhatt.52459/' target="_blank"><FaFacebookSquare size="2rem"/></Link>
+      <Link to='https://instagram.com/galaxy.click_capture/' target="_blank"><FaInstagramSquare size="2rem"/></Link>
       <Link to='https://github.com/anuragbhatt1805/' target="_blank"><FaGithubSquare size="2rem"/></Link>
       </div>
     </div>
