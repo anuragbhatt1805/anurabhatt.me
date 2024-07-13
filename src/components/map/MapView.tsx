12e67f7be5446b88951bb01568c0896cf4116ddd
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-07-14 04:02:43 +0530

    map marker added

--- src/components/map/MapView.tsx
+++ src/components/map/MapView.tsx
@@ -1,6 +1,7 @@
 import { MapContainer, Marker, Popup } from "react-leaflet";
 import { TileLayer } from "react-leaflet/TileLayer";
-import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
 
 export type MapPropType = {
@@ -9,6 +10,8 @@ export type MapPropType = {
 };
 
 export const MapView = (props: MapPropType) => {
+  const icon = L.icon({iconUrl: "/marker-icon-2x.png", iconSize: [20, 30]});
+
   return (
     <MapContainer
       className="rounded-lg m-10"
@@ -19,7 +22,7 @@ export const MapView = (props: MapPropType) => {
       style={{ height: "100%", width: "100%", border: "2px solid black" }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={props?.position}>
+      <Marker position={props?.position} icon={icon}>
         {props?.popup && <Popup>{props?.popup}</Popup>}
       </Marker>
     </MapContainer>
